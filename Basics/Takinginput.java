5dd90e209a399053b6853736d87a60ae48741876
johnmasih <ndj4702@gmail.com>
2025-11-23 00:55:25 +0530

    Added Taking Input File

---
+++ Basics/Takinginput.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Takinginput {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        // for String inputs (only single word)
+        
+//        System.out.println("enter your name :  ");
+//        String name = sc.next();
+//        System.out.println("your name is :  " + name);
+        
+        // for String inputs (only full line/sentense )
+        
+//        System.out.println("enter your name :  ");
+//        String name_1 = sc.nextLine();
+//        System.out.println("your name is :  " + name_1);
+        
+        //for integer input
+
+//        System.out.println("enter the number :  ");
+//        int num = sc.nextInt();
+//        System.out.println("entered number is " + num);
+
+//          System.out.println("enter any two numbers : ");
+//          int num1 = sc.nextInt();
+//          int num2 = sc.nextInt();
+//          
+//          int sum =  num1 + num2 ;
+//           
+//          System.out.println("the sum is  : " + sum);
+
+
+          System.out.println("enter your name :  ");
+            String name = sc.next();
+        System.out.println("your name is :  " + name);
+          
+        
+        
+    }
+    
+}
