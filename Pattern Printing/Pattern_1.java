e80d637a36cf33cd3ea9749a94f83827b11828ea
johnmasih <ndj4702@gmail.com>
2025-11-23 18:52:59 +0530

    Added New Files

---
+++ Pattern Printing/Pattern_1.java
@@ -0,0 +1,20 @@
+package pattern_printing;
+
+import java.util.Scanner;
+
+public class Pattern_1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+
+    }
+}
