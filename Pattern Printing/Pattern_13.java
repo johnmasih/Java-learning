92f3b2560312e9dbf3a719d2f0f3524f315d113f
johnmasih <ndj4702@gmail.com>
2025-11-25 21:32:16 +0530

    Odd number triangle Pattern Added

---
+++ Pattern Printing/Pattern_13.java
@@ -0,0 +1,33 @@
+/*
+    1
+    1 3
+    1 3 5
+    1 3 5 7
+    1 3 5 7 9
+
+n = 5
+ */
+package pattern_printing;
+
+import java.util.Scanner;
+
+public class Pattern_13 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            int a = 1;                   //<- declare it inside outer loop
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(a + " ");
+                a += 2;
+            }
+            System.out.println("");
+
+        }
+
+    }
+}
+// key point is --> Variables declared inside the loop reset (re-initialized) every iteration.
