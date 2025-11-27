ffb4e693fa71b4e7063aaf0d2fd736cdf2252097
johnmasih <ndj4702@gmail.com>
2025-11-27 17:34:44 +0530

    Added New Pattern

---
+++ Pattern Printing/Pattern_14.java
@@ -0,0 +1,43 @@
+/*
+
+          *
+        * *
+      * * *
+    * * * *
+
+n = 4
+ */
+package pattern_printing;
+
+import java.util.Scanner;
+
+public class Pattern_14 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number : ");
+        int n = sc.nextInt();
+//      Method 1
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+        
+//      Method 2
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= n; j++) {   
+//                if((i+j)>n)System.out.print("* ");
+//                else System.out.print("  ");
+//            }
+//            System.out.println("");
+//        }
+        sc.close();
+
+    }
+}
