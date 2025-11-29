46074d13afc5ce50f0e9a93e2b60133889bec822
johnmasih <ndj4702@gmail.com>
2025-11-29 22:50:32 +0530

    Added

--- Methods/BuiltInMethods.java
+++ Methods/BuiltInMethods.java
@@ -3,6 +3,11 @@ package Methods;
 
 public class BuiltInMethods {
     public static void main(String[] args) {
-        System.out.println("");
+        double x = Math.sqrt(16);    // For Square root
+        double y = Math.cbrt(343);   // For Cube root
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(Math.abs(-8.8));  // Math.abs() gives the absolute(non-negative) values
+        
     }
 }
