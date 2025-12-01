14de9ebeb90cc16f58d8bd2bfbf1e6454bc07134
johnmasih <ndj4702@gmail.com>
2025-12-01 21:40:16 +0530

    Added File

--- Methods/SwapTwoNumbers.java
+++ Methods/SwapTwoNumbers.java
@@ -1,23 +1,23 @@
 /*   Demonstrating pass-by-value concept   */
 
-
 package Methods;
+
 import java.util.Scanner;
 
 public class SwapTwoNumbers {
-    
-    public static void swap(int x , int y){
+
+    public static void swap(int x, int y) {
         int temp = x;
         x = y;
         y = temp;
-        System.out.println("After " + x +" " + y);
+        System.out.println("After " + x + " " + y);
     }
-    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.println("Before "+ a +" "  + b);
-        swap(a,b);
+        System.out.println("Before " + a + " " + b);
+        swap(a, b);
     }
 }
