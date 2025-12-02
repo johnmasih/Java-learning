7b503b7b99777ac3215f1dc1003798f3cc6a0738
johnmasih <ndj4702@gmail.com>
2025-12-02 20:42:32 +0530

    Added

--- Arrays/ArrayBasics.java
+++ Arrays/ArrayBasics.java
@@ -4,6 +4,6 @@ package Arrays;
 public class ArrayBasics {
     public static void main(String[] args) {
         int [] arr = {10,20,30,40,50};
-        System.out.println(arr[3]);
+        
     }
 }
