049e5d3d9526f2eea5178eda0e44bb15fc18425b
johnmasih <ndj4702@gmail.com>
2025-12-05 16:45:33 +0530

    For Each Loop

---
+++ Arrays/ForEachLoop.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class ForEachLoop {
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40};
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
