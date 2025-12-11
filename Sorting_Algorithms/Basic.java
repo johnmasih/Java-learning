a20de32305877f894042a8ef958b0cc036c62ca1
johnmasih <ndj4702@gmail.com>
2025-12-11 21:01:33 +0530

    Added

--- Sorting_Algorithms/Basic.java
+++ Sorting_Algorithms/Basic.java
@@ -4,6 +4,6 @@ package Sorting_Algorithms;
 
 public class Basic {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello");
     }
 }
