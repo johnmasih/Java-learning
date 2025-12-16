3eaad2b7cffd854bca6d2a14c60ff90eb7bc795d
johnmasih <ndj4702@gmail.com>
2025-12-16 20:42:07 +0530

    added

--- Sorting_Algorithms/Basic.java
+++ Sorting_Algorithms/Basic.java
@@ -1,22 +1,21 @@
-
 package Sorting_Algorithms;
 
 // Check if array is sorted or not.
-
 public class Basic {
+
     public static void main(String[] args) {
-        int[] arr = {10,20,30,40,50};
+        int[] arr = {10, 20, 30, 40, 50};
         boolean isSorted = true;
-        for(int i =0; i<arr.length-1;i++){
-            if(arr[i]>arr[i+1]){ 
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
                 isSorted = false;
                 break;
             }
         }
-        
-        if(isSorted==true){
+
+        if (isSorted == true) {
             System.out.println("sorted");
-        }else{
+        } else {
             System.out.println("Not sorted");
         }
     }
