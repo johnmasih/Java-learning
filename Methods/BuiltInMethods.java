28743abe3f5d77ff7f5c00e6e438a9ae656f0b0a
johnmasih <ndj4702@gmail.com>
2025-12-26 16:52:58 +0530

    Added

--- Methods/BuiltInMethods.java
+++ Methods/BuiltInMethods.java
@@ -8,7 +8,7 @@ public class BuiltInMethods {
         System.out.println(x);
         System.out.println(y);
         System.out.println(Math.abs(-8.8));  // Math.abs() gives the absolute(non-negative) values.
-        System.out.println(Math.floor(8.3));  // Math.floor(x) returns the greatest integer less than or equal to x.
+        System.out.println(Math.floor(8.3));  // Math.floor(x) returns the greatest integer less than or equal to x
         System.out.println(Math.floor(-3.3)); 
         System.out.println(Math.ceil(8.00001));  // opp. of floor()
         System.out.println(Math.ceil(-8.01)); 
