21ef35965f9500fa71af22332b040278646a459d
johnmasih <ndj4702@gmail.com>
2025-12-31 19:49:02 +0530

    ADDED

--- Pattern Printing/Pattern_4.java
+++ Pattern Printing/Pattern_4.java
@@ -11,7 +11,7 @@ public class Pattern_4 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number: ");
+        System.out.println("Enter number : ");
         int n = sc.nextInt();
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
